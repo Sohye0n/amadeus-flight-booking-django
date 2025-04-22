15ea24bf5e54b4ffc64b59f074de147ffe5b480e
Ubuntu <ubuntu@ip-172-31-25-19.ap-southeast-2.compute.internal>
2025-04-22 03:26:56 +0000

    개발용 브랜치 init

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -a
+source ../flight-booking.env
+set +a
+
+echo "[*] 현재 경로: $(pwd)"
+echo "[*] 현재 python 경로: $(which python)"
+echo "[*] 현재 가상환경: $VIRTUAL_ENV"
+
+source /home/ubuntu/amadeus-flight-booking-django/venv/bin/activate
+echo "[*] 가상환경 활성화 후 python 경로: $(which python)"
+echo "[*] 가상환경 활성화 후 manage.py 존재 여부:"
+
+cd /home/ubuntu/amadeus-flight-booking-django
+python amadeus_demo_api/manage.py runserver 0.0.0.0:8000
