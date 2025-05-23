d079763e888c24a83d61578ca48880b49a63ec02
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-05-24 00:31:48 +0900

    test:aiserver응답방식확인

--- amadeus_demo_api/booking/admin.py
+++ amadeus_demo_api/booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import FlightOrder, FlightSearchRecord, FlightOrderRetrieveLog, FlightCancelLog
+from .models import FlightOrder, FlightSearchRecord, FlightOrderRetrieveLog, FlightCancelLog, FlightOfferCandidate
 
 admin.site.register(FlightOrder)
 admin.site.register(FlightSearchRecord)
 admin.site.register(FlightOrderRetrieveLog)
 admin.site.register(FlightCancelLog)
+admin.site.register(FlightOfferCandidate)
