35ade74a8d03dad5f375d5144b7aa615f1cc5f93
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-05-23 16:48:32 +0900

    feat:mapping-booking

--- amadeus_demo_api/chatbot/admin.py
+++ amadeus_demo_api/chatbot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import ChatHistory
+from .models import ChatHistory,ChatbotFlightActionLog
 
 admin.site.register(ChatHistory)
+admin.site.register(ChatbotFlightActionLog)
