8f2b138a270a416c1174d4818566043a3cff8770
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-06-04 19:35:24 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e  # 오류 시 즉시 종료
 pip install -r requirements.txt
 
 # 서버 실행
-python amadeus_demo_api/manage.py makemigrations
+python amadeus_demo_api/manage.py makemigrations --noinput
 python amadeus_demo_api/manage.py migrate
 exec python amadeus_demo_api/manage.py runserver 0.0.0.0:8000
